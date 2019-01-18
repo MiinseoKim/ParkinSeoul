284ae3b23b8f6864802051d16a317b40b88a9cb4
saekong <miing4@naver.com>
2019-01-18 15:29:31 +0900

    parkdto 생성

---
+++ parkinseoul/src/main/java/com/parkinseoul/dto/ParkDto.java
@@ -0,0 +1,24 @@
+package com.parkinseoul.dto;
+
+public class ParkDto {
+  private int P_IDX;                //공원 번호
+  private String P_PARK;            //공원 이름
+  private String P_LIST_CONTENT;    //공원 설명
+  private String AREA;              //공원 면적
+  private String OPEN_DT;           //공원 설립일
+  private String MAIN_EQUIP;        //
+  private String MAIN_PLANTS;
+  private String GUIDANCE;
+  private String VISIT_ROAD;
+  private String USE_REFER;
+  private String P_IMG;
+  private String P_ZONE;
+  private String P_ADDR;
+  private String P_ADMINTEL;
+  private String G_LONGITUDE;
+  private String G_LATITUDE;
+  private String LONGITUDE;
+  private String LATITUDE;
+  private String TEMPLATE_URL;    //홈페이
+
+}
